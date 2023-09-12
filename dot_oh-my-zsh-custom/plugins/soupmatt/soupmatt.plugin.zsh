8b14f3a21c0ecb1c55e7382750272274e9eacf37
Matt Campbell <matt.campbell@joinhandshake.com>
2023-09-12 10:25:23 -0500

    remove old stuff from soupmatt plugin

--- dot_oh-my-zsh-custom/plugins/soupmatt/soupmatt.plugin.zsh
+++ dot_oh-my-zsh-custom/plugins/soupmatt/soupmatt.plugin.zsh
@@ -1,7 +1,1 @@
-cs() { cd ~/src/github.com/Shopify/$1; }
-_cs() { _files -W ~/src/github.com/Shopify -/;  }
-compdef _cv cv
-
-cg() { cd ~/src/github.com/$1; }
-_cg() { _files -W ~/src/github.com -/;  }
-compdef _co co
+# keep me
