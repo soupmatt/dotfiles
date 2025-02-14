3f555b3a21dd9e346cf4b3f1b900fb4e6c7d80e3
Matt Campbell <matt.campbell@shopify.com>
2025-02-14 16:05:31 -0600

    Job change!
    Swap out handshake specific things for shopify specific things

--- dot_oh-my-zsh-custom/plugins/soupmatt/soupmatt.plugin.zsh
+++ dot_oh-my-zsh-custom/plugins/soupmatt/soupmatt.plugin.zsh
@@ -1,1 +1,7 @@
-# keep me
+cs() { cd ~/src/github.com/Shopify/$1; }
+_cs() { _files -W ~/src/github.com/Shopify -/;  }
+compdef _cv cv
+
+cg() { cd ~/src/github.com/$1; }
+_cg() { _files -W ~/src/github.com -/;  }
+compdef _co co
